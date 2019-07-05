32ed08498cfae24ac4294c8728312062bfdccbb5
James Bouy <jbouy@turner-industries.com>
2019-07-04 20:44:48 -0500

    s15l150: Array Data in Firebase: Part II

--- expensify/src/firebase/firebase.js
+++ expensify/src/firebase/firebase.js
@@ -1,35 +1,117 @@
 import * as firebase from "firebase";
+import moment from "moment";
 
-const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};
+const firebaseConfig = {};
 
 firebase.initializeApp(firebaseConfig);
 
-// const database = firebase.database();
+const database = firebase.database();
 
-// database.ref().set({
-//   name: "James Bouy",
-//   age: 39,
-//   isSingle: false,
-//   location: {
-//     city: "Addis",
-//     country: "United States"
-//   }
+database.ref("expenses").on("child_removed", snapshot => {
+  console.log(snapshot.key, snapshot.val());
+});
+
+database.ref("expenses").on("child_changed", snapshot => {
+  console.log(snapshot.key, snapshot.val());
+});
+
+// database
+//   .ref("expenses")
+//   .once("value")
+//   .then(snapshot => {
+//     const expenses = [];
+
+//     snapshot.forEach(childSnapshot => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       });
+//     });
+
+//     console.log(expenses);
+//   });
+
+// database.ref("expenses").on("value", snapshot => {
+//   const expenses = [];
+
+//   snapshot.forEach(childSnapshot => {
+//     expenses.push({
+//       id: childSnapshot.key,
+//       ...childSnapshot.val()
+//     });
+//   });
+
+//   console.log(expenses);
 // });
 
-// //database.ref().set("This is my data.");
-
-// database.ref("age").set(40);
-// database.ref("location/city").set("Baton Rouge");
-
-// database.ref("attributes").set({
-//   height: 71,
-//   weight: 180
+// database.ref("expenses").push({
+//   description: "Test 1 Description",
+//   note: "Test 1",
+//   amount: 1,
+//   createdAt: moment().valueOf()
 // });
+
+// database.ref("notes").push({
+//   title: "Course Topics",
+//   body: "React Native, Angular, Python"
+// });
+
+// database.ref().on("value", snapshot => {
+//   const val = snapshot.val();
+//   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}.`);
+// });
+
+// setTimeout(() => {
+//   database.ref().update({
+//     "job/company": "Google"
+//   });
+// }, 3500);
+
+// database
+//   .ref("location/city")
+//   .once("value")
+//   .then(snapshot => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch(e => {
+//     console.log("Error fetching data", e);
+//   });
+
+// database
+//   .ref()
+//   .set({
+//     name: "James Bouy",
+//     age: 39,
+//     stressLevel: 6,
+//     job: {
+//       title: "Software developer",
+//       company: "Google"
+//     },
+//     location: {
+//       city: "Addis",
+//       country: "United States"
+//     }
+//   })
+//   .then(() => {
+//     console.log("Data is saved.");
+//   })
+//   .catch(error => {
+//     console.log("This failed", error);
+//   });
+
+// database.ref().update({
+//   stressLevel: 9,
+//   "job/company": "Amazon",
+//   "location/city": "Seattle"
+// });
+
+// database
+//   .ref()
+//   .remove()
+//   .then(() => {
+//     console.log("Removed `isSingle`");
+//   })
+//   .catch(error => {
+//     console.log("Could not remove `isSingle`", error);
+//   });
--- expensify/src/playground/promises.js
+++ expensify/src/playground/promises.js
@@ -1,11 +1,21 @@
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("This is my resolved data");
-  }, 1500);
+    // resolve({
+    //   name: "James",
+    //   age: 39
+    // });
+    reject("Something went wrong!");
+  }, 5000);
 });
 
 console.log("Before");
-promise.then(data => {
-  console.log(data);
-});
+
+promise
+  .then(data => {
+    console.log("1", data);
+  })
+  .catch(error => {
+    console.log("error:", error);
+  });
+
 console.log("After");
